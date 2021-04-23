dea7973f79d7312c83c8b7481202dda482caef7b
rajaabhaysingh <rajaabhaysingh233@gmail.com>
2021-04-23 23:29:41 +0530

    Caching with fav implemented

--- src/redux/reducers/bank.reducers.js
+++ src/redux/reducers/bank.reducers.js
@@ -2,9 +2,101 @@ import { bankConstants } from "../actions/constants";
 
 const initialState = {
   getBankBranchesData: [],
+  getBankBranchesFavData: [],
   getBankBranchesLoading: false,
   getBankBranchesSuccess: false,
   getBankBranchesError: null,
+};
+
+// addFavOptionToInitialData
+const addFavOptionToInitialData = (bankBranchList, city) => {
+  let tempBankBranchList = [];
+
+  // access previously cached data
+  let prevCache = JSON.parse(localStorage.getItem("bankData"));
+
+  if (prevCache && prevCache[city]) {
+    tempBankBranchList = prevCache[city];
+  } else {
+    for (const bankBranch of bankBranchList) {
+      tempBankBranchList.push({
+        fav: false,
+        bank_name: bankBranch.bank_name,
+        branch: bankBranch.branch,
+        ifsc: bankBranch.ifsc,
+        bank_id: bankBranch.bank_id,
+        address: bankBranch.address,
+        district: bankBranch.district,
+        city: bankBranch.city,
+        state: bankBranch.state,
+      });
+    }
+  }
+
+  if (prevCache) {
+    // add new data to temp cache variable
+    prevCache[city] = tempBankBranchList;
+  } else {
+    prevCache = {};
+    prevCache[city] = tempBankBranchList;
+  }
+
+  // remove previously cached data
+  localStorage.removeItem("bankData");
+
+  // re-write new cache
+  localStorage.setItem("bankData", JSON.stringify(prevCache));
+
+  return tempBankBranchList;
+};
+
+// updateDataWithFavList
+const updateDataWithFavList = (oldBankList, ifsc, type, city) => {
+  let tempBankList = oldBankList;
+
+  let foundIndex = null;
+
+  for (let i = 0; i < tempBankList.length; i++) {
+    if (tempBankList[i].ifsc === ifsc) {
+      foundIndex = i;
+    }
+  }
+
+  // if index found
+  if (foundIndex) {
+    if (type === "add") {
+      tempBankList[foundIndex].fav = true;
+    } else {
+      tempBankList[foundIndex].fav = false;
+    }
+  } else {
+    if (foundIndex === 0) {
+      if (type === "add") {
+        tempBankList[foundIndex].fav = true;
+      } else {
+        tempBankList[foundIndex].fav = false;
+      }
+    }
+  }
+
+  // access previously cached data
+  let prevCache = JSON.parse(localStorage.getItem("bankData"));
+
+  if (prevCache) {
+    // add new data to temp cache variable
+    prevCache[city] = tempBankList;
+  } else {
+    prevCache = {};
+    prevCache[city] = tempBankList;
+  }
+
+  // remove previously cached data
+  localStorage.removeItem("bankData");
+
+  // re-write new cache
+  localStorage.setItem("bankData", JSON.stringify(prevCache));
+
+  return tempBankList;
 };
 
 export default (state = initialState, action) => {
@@ -20,7 +112,10 @@ export default (state = initialState, action) => {
     case bankConstants.GET_BANK_BRANCHES_SUCCESS:
       state = {
         ...state,
-        getBankBranchesData: action.payload.data,
+        getBankBranchesData: addFavOptionToInitialData(
+          action.payload.data,
+          action.payload.city
+        ),
         getBankBranchesLoading: false,
         getBankBranchesSuccess: true,
         getBankBranchesError: null,
@@ -34,6 +129,18 @@ export default (state = initialState, action) => {
         getBankBranchesLoading: false,
         getBankBranchesSuccess: false,
         getBankBranchesError: action.payload.error,
+      };
+      break;
+
+    case bankConstants.ADD_TO_FAV_LIST_SUCCESS:
+      state = {
+        ...state,
+        getBankBranchesData: updateDataWithFavList(
+          state.getBankBranchesData,
+          action.payload.ifsc,
+          action.payload.type,
+          action.payload.city
+        ),
       };
       break;
 
